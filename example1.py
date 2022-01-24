9af6a3d271f7592a339ba326f804acc838f994da
luoyi <none>
2022-01-24 22:31:38 +0800

    update example1

--- example1.py
+++ example1.py
@@ -29,8 +29,9 @@ ani_func.cmds_list = []
 curve = shape.bezier3x_xyz([[21, 20, 20], [38, 28, 8], [46, 19, 10], [45, 23, -8]], 0.2)
 particle_cmd.static_particle(0, 40, curve, 'end_rod', 0, 0, 0, 0, 1)
 ani_func.add_cmd(particle_cmd.cmds)
-ani_func.output_cb_seq_function('mcae', 'curve', -10, 7, -10, 'x+', 64, 64)
-ani_func.save_seq_file('curve')
+# ani_func.output_cb_seq_function('mcae', 'curve', -10, 7, -10, 'x+', 64, 64)
+# ani_func.save_seq_file('curve')
+ani_func.save_seq_file('ani', build_schedule=True, namespace='mcae')
 
 # 围绕上面的贝塞尔曲线绘制螺线
 particle_cmd.cmds = []
@@ -40,8 +41,9 @@ helix = shape.helix(21, 20, 20, 45, 23, -8, 3, 0.1,
 # 这次换点不一样子粒子
 particle_cmd.static_particle(0, 40, helix, 'end_rod', 0.1, 0.1, 0.1, 0.03, 3)
 ani_func.add_cmd(particle_cmd.cmds)
-ani_func.output_cb_seq_function('mcae', 'helix', -10, 9, -10, 'x+', 64, 64)
-ani_func.save_seq_file('helix')
+# ani_func.output_cb_seq_function('mcae', 'helix', -10, 9, -10, 'x+', 64, 64)
+# ani_func.save_seq_file('helix')
+ani_func.save_seq_file('ani', build_schedule=True, namespace='mcae')
 
 # 单独绘制一条围绕抛物线的螺线
 particle_cmd.cmds = []
@@ -51,5 +53,6 @@ helix = shape.helix(21, 20, 20, 100, 20, 100, 7, 0.1,
 
 particle_cmd.static_particle(0, 40, helix, 'firework', 0.2, 0.2, 0.2, 0.1, 7)
 ani_func.add_cmd(particle_cmd.cmds)
-ani_func.output_cb_seq_function('mcae', 'helix2', -10, 11, -10, 'x+', 64, 64)
-ani_func.save_seq_file('helix2')
+# ani_func.output_cb_seq_function('mcae', 'helix2', -10, 11, -10, 'x+', 64, 64)
+# ani_func.save_seq_file('helix2')
+ani_func.save_seq_file('ani', build_schedule=True, namespace='mcae')
