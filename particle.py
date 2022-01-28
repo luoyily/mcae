2983bd06d262f59ce86b6fb93632618b99fe9f89
An-Yan-d <75794721+An-Yan-d@users.noreply.github.com>
2022-01-28 16:29:56 +0800

    Update particle.py
    增加以参数方程生成命令
    增加函数方式分割粒子序列

--- particle.py
+++ particle.py
@@ -27,22 +27,36 @@ class CmdBuilder:
         self.cmds = []
         self.temp_cmds = []
 
-    def average_cmd_count(self, count, tick):
-        """计算平均每tick指令数"""
+    def cls(self):
+        self.cmds = []
+
+    def average_cmd_count(self, count, t0 , t1 , fun):
+        """计算每tick指令数"""
         count_list = []
+        tick=int(t1-t0+1)
+        if fun:
+            sum=0
+            for t in range(t0,t1+1):
+                n=fun(t)
+                count_list.append(n*count)
+                sum+=n
+            for t in range(0, tick):
+                count_list[t]//=sum
+
+        else:
         # 整除计算每tick需要的数量
-        i = count // tick
+            i = count // tick
         # 计算余数，之后平均插入
-        r = count % tick
-        for n in range(0, tick):
-            count_list.append(i)
+            r = count % tick
+            for n in range(0, tick):
+                count_list.append(i)
         # 将余下的点平均插入
-        for n in range(0, int(r)):
-            if round(tick / r) * n < len(count_list):
-                count_list[round(tick / r) * n] = count_list[round(tick / r) * n] + 1
+            for n in range(0, int(r)):
+                if round(tick / r) * n < len(count_list):
+                    count_list[round(tick / r) * n] += 1
         return count_list
 
-    def cmds_to_seq(self, t0, t1, cmds):
+    def cmds_to_seq(self, t0, t1, fun=None):
         """
         将指令列表转为t0-t1 Tick的序列并添加到总列表中
         :param t0: 起始刻
@@ -50,12 +64,13 @@ class CmdBuilder:
         :param cmds: 指令列表
         :return: None
         """
+        cmds=self.temp_cmds
         if t0 == t1:
             for cmd in cmds:
                 cmd.tick = t0
                 self.cmds.append(cmd)
         else:
-            count_list = self.average_cmd_count(len(cmds), int(t1-t0+1))
+            count_list = self.average_cmd_count(len(cmds), t0,t1,fun)
             # 按每tick粒子数量切片列表
             index1, index2 = 0, 0
             for i, c in enumerate(count_list):
@@ -64,15 +79,15 @@ class CmdBuilder:
                     cmd.tick = t0 + i
                     self.cmds.append(cmd)
                 index1 = index2
+        self.temp_cmds=[]
 
     def static_particle(self, t0, t1, points, name, dx, dy, dz, speed, amount):
         """生成普通粒子序列"""
         self.temp_cmds = []
         for p in points:
-            x, y, z = p[0], p[1], p[2]
-            cmd = Command(t0, name, x, y, z, dx, dy, dz, speed, amount)
+            cmd = Command(t0, name, p[0],p[1],p[2], dx, dy, dz, speed, amount)
             self.temp_cmds.append(cmd)
-        self.cmds_to_seq(t0, t1, self.temp_cmds)
+        self.cmds_to_seq(t0, t1)
 
     def motion_particle(self, points, motions, t0, t1, name, speed):
         """生成带初速度的粒子序列，每个粒子具有指定初速度"""
@@ -82,7 +97,7 @@ class CmdBuilder:
             dx, dy, dz = m[0], m[1], m[2]
             cmd = Command(t0, name, x, y, z, dx, dy, dz, speed, 0)
             self.temp_cmds.append(cmd)
-        self.cmds_to_seq(t0, t1, self.temp_cmds)
+        self.cmds_to_seq(t0, t1)
 
     def motion_move(self, points_1, points_2, t0, t1, name, speed, zoom=11):
         """生成粒子移动动画"""
@@ -144,7 +159,7 @@ class CmdBuilder:
                     points.append([x, y, z])
         self.static_particle(t0, t1, points, particle_name, 0, 0, 0, 0, 1)
 
-    def color_particle_img(self, filename, x0, y0, z0, zoom_level, t0, t1, particle_size,
+    def color_particle_img(self, filename, x0, y0, z0, t0, t1, particle_size, zoom_level=7,
                            is_rotate=False, vec1=None, vec2=None, degree=None):
         """
         生成彩色粒子画（1.13+）
@@ -152,10 +167,10 @@ class CmdBuilder:
         :param x0: 图片左上角坐标 x
         :param y0: 图片左上角坐标 y
         :param z0: 图片左上角坐标 z
-        :param zoom_level: 缩放级别，整数（推荐7）
         :param t0: 起始Tick
         :param t1: 结束Tick
         :param particle_size: 粒子大小
+        :param zoom_level: 缩放级别，整数（推荐7）
         :param is_rotate: 是否旋转
         :param vec1: 旋转轴起点
         :param vec2: 旋转轴终点
@@ -184,7 +199,42 @@ class CmdBuilder:
                 else:
                     cmd = Command(t0, name, x, y, z, 0, 0, 0, 0, 1)
                     self.temp_cmds.append(cmd)
-        self.cmds_to_seq(t0, t1, self.temp_cmds)
+        self.cmds_to_seq(t0, t1)
+
+
+
+    def cmds_fun(self, t0, t1, fun, name, dx, dy, dz, speed, amount ,ppt=3,frags=None):
+        """
+        以参数方程形式生成命令
+        :param t0:起始tick
+        :param t1:终止tick
+        :param fun:坐标函数，参数格式为（t，*frags），返回值为x，y，z
+        :param frags:需要传入fun的参数，用元组表示，如fun的参数为（t,a,b,c），则frags=（a,b,c）
+        :param ppt:每tick点数,default:3
+        """
+        dt=1/ppt
+        for tick in range(t0,t1):
+            for i in range(ppt):
+                if not frags:
+                    x,y,z=fun(tick+i*dt)
+                else:
+                    x,y,z=fun(tick+i*dt,*frags)
+                cmd = Command(tick, name, x, y, z, dx, dy, dz, speed, amount)
+                self.cmds.append(cmd)
+    
+    def static_particle_fun(self, t0, t1, points, name, dx, dy, dz, speed, amount,fun):
+        """
+        生成普通粒子序列,时间的浓度方程版
+        :param fun:时间浓度方程，参数为t，返回值为t时刻轨迹速度
+
+        """
+        self.temp_cmds = []
+        for p in points:
+            cmd = Command(t0, name, p[0],p[1],p[2], dx, dy, dz, speed, amount)
+            self.temp_cmds.append(cmd)
+        self.cmds_to_seq(t0, t1 ,fun)
+
+
 
 
 
